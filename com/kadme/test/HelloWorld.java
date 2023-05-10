abf80d72eb65833efcb471d8645f2268d0fed375
DribDrob <drobushenko@gmail.com>
2023-05-11 00:29:36 +0200

    instantiate

---
+++ com/kadme/test/HelloWorld.java
@@ -0,0 +1,31 @@
+package com.kadme.test;
+
+import java.awt.Point;
+// import java.awt.Polygon;
+// import java.util.*;
+// import java.util.ArrayList;
+// import java.util.List;
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        Point point1 = new Point(0, 0);
+        System.out.println(point1.getX());
+        System.out.println(point1.getY());
+
+        Point point2 = new Point(1, 1);
+        System.out.println(point2.getX());
+        System.out.println(point2.getY());
+
+        // Line l1 = new Line(new Point(2, 3), new Point(4, 6));
+        // ;
+
+        // List<Point> pointList = new ArrayList<Point>();
+        // pointList.add(p1);
+        // pointList.add(p2);
+        // Polygon polygon1 = new Polygon();
+
+        // Set<Point> pointHashSet = new HashSet<Point>();
+
+        System.out.println("Hello World");
+    }
+}
